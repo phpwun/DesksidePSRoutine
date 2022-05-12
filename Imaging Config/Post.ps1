359fc06ed5c5a91ec929bc4c8a2bbe3566fd0518
Anthony Ritter <aritterdls@gmail.com>
2022-05-12 14:29:14 -0500

    Sanitized Data in Files
    Security risks minimized

--- Imaging Config/Post.ps1
+++ Imaging Config/Post.ps1
@@ -2,7 +2,7 @@
 Remove-Item 'C:\Windows\System32\Recovery\ReAgent.xml' -Force -Confirm
 
 # Enable-Bitlocker cmdlet requires a SecureString for its pin, so we provide the pin in plaintext to be converted to a SecureStrong Object.
-$SecureString = ConvertTo-SecureString "4257" -AsPlainText -Force
+$SecureString = ConvertTo-SecureString "PIN#" -AsPlainText -Force
 
 # Enables bitlocker on the C drive and specifies Aes256 as the encryption type, tells the cmdlet to only encypt used space and pipes in the,
 # securestring then species that the user should be able to use the TPM and a Pin to unlock bitlocker.
