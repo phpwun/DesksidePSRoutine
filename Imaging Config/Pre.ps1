716a6e5fb6ad195aaea3fbfa186ea236b341dd39
Anthony Ritter <anritter@heartlandalliance.org>
2022-05-12 15:41:19 -0500

    Removed post restart script and reconfigured pre restart script to include components
    Post script was being very buggy and the process it replaces can be finished in a few seconds either way, will re-code later

--- Imaging Config/Pre.ps1
+++ Imaging Config/Pre.ps1
@@ -1,35 +1,37 @@
 # Part 1
-# Somewhat self explanitory, copies a folder to temp from a USB Drive
-Copy-Item -Path "D:\copy 2 temp in C" -Destination "C:\temp" -recurse -Force
+# Somewhat self explanitory, find which USB drive conatains it then copies a folder to temp from a USB Drive
+If (Test-Path E:)
+    {
+    Copy-Item -Path "E:\copy 2 temp in C" -Destination "C:\temp" -recurse -Force
+    }
+else { Copy-Item -Path "D:\copy 2 temp in C" -Destination "C:\temp" -recurse -Force }
 
 # Begins a software installation via msiexec, specifies that it should wait till done to start the next cmdlet and then,
 # supplies the installer path and that the installer should run as normal /I, and that it should run with no userinput /quietand specifies passthru so the output can be viewed if needed
-Start-Process msiexec.exe -Wait -ArgumentList '/I C:\temp\(Remote Software Name Goes Here).MSI /quiet' -PassThru
-Write-Output "Finished."
+#Start-Process msiexec.exe -Wait -ArgumentList '/I C:\temp\copy 2 temp in C\Agent_Install_Heartland.MSI /quiet' -PassThru
+#Write-Output "Agent_Install_Heartland."
 
 # Same command as above without msiexe because this is not an msi installer, provides the silent argument differently.
-Start-Process -Wait -FilePath 'C:\temp\(Remote Software Name 2 Goes Here).exe' -ArgumentList "/quiet" -PassThru
+Start-Process -Wait -FilePath 'C:\temp\copy 2 temp in C\Symantec Client.exe' -ArgumentList "/quiet" -PassThru
 Write-Output "Finished." 
 
 #Installs Chrome
-Invoke-WebRequest -Uri "https://dl.google.com/chrome/install/ChromeStandaloneSetup64.exe" -OutFile "C:\temp\copy 2 temp in C\ChromeStandaloneSetup64.exe"; & -Wait "C:\temp\copy 2 temp in C\ChromeStandaloneSetup64.exe" /silent /install
+$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+Write-Output "Finished." 
 
 # Part 2
 #defines the architecture of the device as either 32 or 64 bit
 function Get-Architecture {
-	
 	[CmdletBinding()]
 	param ()
-	
 	$Architecture = Get-WmiObject -Class Win32_OperatingSystem | Select-Object OSArchitecture
 	$Architecture = $Architecture.OSArchitecture
-	#Returns 32-bit or 64-bit
 	Return $Architecture
 }
 
 #Uses the architecture defenition to find the dellcommandupdate executable and returns the file
 function Get-DellCommandUpdateLocation {
-	
+$OvRide = $true
 	$Architecture = Get-Architecture
 	If ($Architecture -eq "32-bit" -Or $OvRide -eq "true") {
 		$File = Get-ChildItem -Path $env:ProgramFiles -Filter "dcu-cli.exe" -ErrorAction SilentlyContinue -Recurse
@@ -41,7 +43,7 @@ function Get-DellCommandUpdateLocation {
 
 #Executes the functions and attempts to begin the update with the policy
 $a=Get-DellCommandUpdateLocation
-& $a /configure silent -autoSuspendBitLocker=enable -userConsent=disable
+& $a /configure silent '-autoSuspendBitLocker=enable -userConsent=disable'
 & $a /scan -outputLog='C:\dell\logs\scan.log'
 & $a /applyUpdates -outputLog='C:\dell\logs\applyUpdates.log'
 
@@ -50,8 +52,12 @@ $a=Get-DellCommandUpdateLocation
 $Name = (Get-WmiObject -class win32_bios).SerialNumber
 
 # Attributes a custom string to the defenition Domain, this could be the domain server address
-$Domain = "Domain.Goes.Here"
+#$Domain = "cho.ha.local"
 
 # Pipes Name and Domain into Add-Computer via NewName and DomainName to assign the device with,
 # a name based off the serial number and a domain based off of the string defined above.
-Add-Computer -NewName $Name -DomainName $Domain -Restart -PassThru -Force -Credential User
+Remove-Item 'C:\Windows\System32\Recovery\ReAgent.xml' -Force -Confirm -Confirm:$false
+Rename-Computer -NewName $Name
+
+
+#Add-Computer -NewName $Name -DomainName $Domain -Restart -PassThru -Force -Credential User
