5c0e02d1ea3da4eeb63c371aca0a474fbcaddecb
Anthony Ritter <phpwun@gmail.com>
2022-06-23 17:42:28 -0500

    Reformating

--- HA Deskside Script/Main.ps1
+++
@@ -1,7 +0,0 @@
-# Effectivley needed to overhaul the previous version because of how much stuff I was adding to very specific side scripts
-function Get-Architecture {
-	[CmdletBinding()]
-	param ()
-	$Architecture = Get-WmiObject -Class Win32_OperatingSystem | Select-Object OSArchitecture | Write-Output $Architecture.OSArchitecture
-}
-Get-Architecture
